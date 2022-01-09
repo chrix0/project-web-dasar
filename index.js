f9befb2b62436cad9fe5267bfa3f44b1cb6bdbeb
chrix0 <christiantopratama@gmail.com>
2022-01-10 00:10:32 +0700

    Masonry + ImageLoaded fix

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-var container = document.querySelector('.masonry-grid');
-var msnry = new Masonry( container, {
-  // options
-  columnWidth: 0,
-  itemSelector: '.grid-item',
-  percentPosition: true
-});
 
-console.log(
-  "index.js sudah jalan"
-)
+
